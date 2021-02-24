b822628a34aa8bcd798ce997b5d7e83fbda15fbc
Anastasiia <anastasiia.naziiuta@gmail.com>
2021-02-24 15:34:28 +0200

    add media

--- src/js/script.js
+++ src/js/script.js
@@ -18,5 +18,29 @@
             }
         showPageUp();
 
+        //Mobile menu
+
+        let mobileMenu = document.querySelector('#js-navigation'),
+        openMenu = document.querySelector('#js-menu'),
+        closeMenu = document.querySelector('.js-close');
+
+        openMenu.addEventListener('click', () =>{
+            if ( window.innerWidth <=769 ) {
+                mobileMenu.classList.add('header__menu-items--active');
+                openMenu.style.display = 'none';
+                closeMenu.style.display = 'block';
+            }
+
+        });
+
+        closeMenu.addEventListener('click', () => {
+            mobileMenu.classList.remove('header__menu-items--active');
+            closeMenu.style.display = 'none';
+            openMenu.style.display = 'block';
+            
+        });
+    
+
+
         new WOW().init();
     }); 
